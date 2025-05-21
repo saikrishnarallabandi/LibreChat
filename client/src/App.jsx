fa3322433bb56f4b3f959b8aea125eaeadbdfdb3
saikrishnarallabandi <srallaba.research@gmail.com>
2025-05-21 00:00:34 -0500

    Removed the trailing RunCodeTool from

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,6 @@ import { ToastProvider } from './Providers';
 import Toast from './components/ui/Toast';
 import { LiveAnnouncer } from '~/a11y';
 import { router } from './routes';
-import RunCodeTool from './components/RunCodeTool';
 
 
 const App = () => {
@@ -35,7 +34,6 @@ const App = () => {
               <ToastProvider>
                 <DndProvider backend={HTML5Backend}>
                   <RouterProvider router={router} />
-                  <RunCodeTool />
                   <ReactQueryDevtools initialIsOpen={false} position="top-right" />
                   <Toast />
                   <RadixToast.Viewport className="pointer-events-none fixed inset-0 z-[1000] mx-auto my-2 flex max-w-[560px] flex-col items-stretch justify-start md:pb-5" />
